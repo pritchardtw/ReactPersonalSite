fc94ea8bc86df30fc8c05036b6b9c96a78ac9060
Thomas Pritchard <thomas@Thomass-MacBook-Pro.local>
2017-08-28 23:24:32 -0500

    Updated styling g and about me section

--- src/components/Home/aboutsection.js
+++ src/components/Home/aboutsection.js
@@ -8,7 +8,7 @@ export default class AboutSection extends Component {
         <h1 className="about">About!</h1>
         <h2>
         I am an engineer. <br/><br/>
-        My Mission is life long learning and continuous improvement. <br/><br/>
+        My mission is life long learning and continuous improvement. <br/><br/>
         Most of my time is spent studying web and app development, running, skateboarding, and being outdoors.
         </h2>
       </div>
