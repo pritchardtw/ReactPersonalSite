913bed7376e2c9903328290e17ed261ef1ee4614
Thomas Pritchard <thomas@Thomass-MacBook-Pro.local>
2017-08-29 22:37:43 -0500

    Updated images to look clickable when hovered over

--- src/components/pictureviewer.js
+++ src/components/pictureviewer.js
@@ -16,7 +16,7 @@ export default class PictureViewer extends Component {
     return _.map(this.props.images, (image, index) => {
       return (
         <img
-          className={`img-responsive ${this.props.pictureStyle}`}
+          className={`img-responsive clickable ${this.props.pictureStyle}`}
           id={index.toString()}
           src={image}
           onClick={(event) => {
