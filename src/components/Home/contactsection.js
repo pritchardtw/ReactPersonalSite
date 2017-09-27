0e44caf775ef690ff407d88348ec94830b2fddbf
Thomas Pritchard <thomas@Thomass-MacBook-Pro.local>
2017-09-27 15:27:23 -0500

    Updated contact section so clicking e-mail opens e-mail to me

--- src/components/Home/contactsection.js
+++ src/components/Home/contactsection.js
@@ -6,7 +6,7 @@ export default class ContactSection extends Component {
       <div className="content contact">
         <a id="ContactSection"/>
         <h1>Thomas Pritchard</h1>
-        <h2>pritchardtw@gmail.com</h2>
+        <h2><a className="e-mail-link" href="mailto:pritchardtw@gmail.com">pritchardtw@gmail.com</a></h2>
         <h2>262.302.0601</h2>
       </div>
     );
