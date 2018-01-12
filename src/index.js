a81b7763fb5e402659b508d02b067c57cff80d81
Thomas Pritchard <pritchardtw@gmail.com>
2018-01-12 12:49:52 -0700

    Updated package.json to remove redux dependencies. This project doens't use redux. This will hopefully speed up deploy time.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
 import withTracker from './components/withTracker';
 import App from './components/Home/app';
 import Embedded from './components/Embedded/embedded';
 import Application from './components/Application/application';
 import Web from './components/Web/web';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Switch>
@@ -25,5 +19,4 @@ ReactDOM.render(
         </Switch>
       </div>
     </BrowserRouter>
-  </Provider>
   , document.querySelector('.initial-container'));
